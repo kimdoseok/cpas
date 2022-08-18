3453780ec51287a1368db7417846a66f88368c4e
Doseok Kim <doseok@madang.com>
2022-08-18 00:07:02 -0400

    added rabbitmq sample apps

--- fwatch/main.go
+++ fwatch/main.go
@@ -2,13 +2,138 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
 
 	g "github.com/AllenDang/giu"
+	"github.com/fsnotify/fsnotify"
 	"github.com/getlantern/systray"
+	"github.com/tidwall/buntdb"
+)
+
+const (
+	root_folder string = "/Users/doseo/Downloads"
+)
+
+var (
+	db   *buntdb.DB
+	dirs []string
 )
 
 func main() {
+	//db, err := buntdb.Open(":memory:")
+	//db, err = buntdb.Open("data.db")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	defer db.Close()
+	FindDirs(root_folder)
 	systray.Run(onReady, onExit)
+}
+
+func DirWatch(dirname string) {
+	// Create new watcher.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Start listening for events.
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Events:
+				//log.Println("event:", ev)
+				//fmt.Println(1, ev.Name)
+				//fmt.Println(2, ev.Op)
+				//fmt.Println(3, ev.String())
+				//fmt.Println(4, ev.Op.String())
+				if ev.Op.String() == "REMOVE" {
+					fmt.Println(ev.Name + " is removed!!!!!")
+				} else if ev.Op.String() == "RENAME" {
+					fmt.Println(ev.Name + " is renamed!!!!!")
+				} else if ev.Op.String() == "CREATE" {
+					fmt.Println(ev.Name + " is created!!!!!")
+				} else if ev.Op.String() == "WRITE" {
+					fmt.Println(ev.Name + " is written!!!!!")
+				} else if ev.Op.String() == "CHMOD" {
+					fmt.Println(ev.Name + " is chmoded!!!!!")
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+
+}
+
+func FindDirs(path string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	go DirWatch(path)
+	for _, f := range files {
+		if f.IsDir() {
+			fpath := path + "/" + f.Name()
+			dirs = append(dirs, fpath)
+			fmt.Println(fpath)
+			go DirWatch(fpath)
+			//FindDirs(fpath)
+		}
+	}
+}
+func walkitem(path string, info fs.FileInfo, err error) error {
+	/*
+		err := db.Update(func(tx *buntdb.Tx) error {
+			_, _, err := tx.Set("mykey", "myvalue", nil)
+			return err
+		})
+		path := "./path/to/fileOrDir"
+		fileInfo, err := os.Stat(path)
+		if err != nil {
+			// error handling
+		}
+
+		if fileInfo.IsDir() {
+			// is a directory
+		} else {
+			// is not a directory
+		}
+
+		err := db.View(func(tx *buntdb.Tx) error {
+			err := tx.Ascend("", func(key, value string) bool {
+				fmt.Printf("key: %s, value: %s\n", key, value)
+				return true // continue iteration
+			})
+			return err
+		})
+
+		var delkeys []string
+		tx.AscendKeys("object:*", func(k, v string) bool {
+			if someCondition(k) == true {
+				delkeys = append(delkeys, k)
+			}
+			return true // continue
+		})
+		for _, k := range delkeys {
+			if _, err = tx.Delete(k); err != nil {
+				return err
+			}
+		}
+	*/
+	return nil
 }
 
 func onReady() {
---
+++ receive/rabbit.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+
+	"github.com/streadway/amqp"
+)
+
+
+type (
+	Message struct {
+		UserID string `json:"userid" `
+		Key    string `json:"key" `
+		Body   string `json:"body" `
+	}
+)
+
+
+func main() {
+
+
+	caCert, err := ioutil.ReadFile("../cert/ca_certificate.pem")
+	if err != nil {
+		fmt.Println("Error1:", err)
+	}
+ 
+	cert, err := tls.LoadX509KeyPair("../cert/client_certificate.pem", "../cert/client_key.pem")
+	if err != nil {
+		fmt.Println("Error2:", err)
+	}
+ 
+	rootCAs := x509.NewCertPool()
+	rootCAs.AppendCertsFromPEM(caCert)
+ 
+	tlsConf := &tls.Config{
+		RootCAs:      rootCAs,
+		Certificates: []tls.Certificate{cert},
+		ServerName:   "localhost", // Optional
+	}
+ 
+	conn, err := amqp.DialTLS("amqps://doseok:kim7795004@localhost:5671/", tlsConf)
+	if err != nil {
+		fmt.Println("Error3:", err)
+	}
+	fmt.Println("Connection:", conn)
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Error4:", err)
+	}
+	
+	
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"imgs_topic", // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	if err != nil {
+		fmt.Println("Error5:", err)
+	}
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+
+	if err != nil {
+		fmt.Println("Error6:", err)
+	}
+
+	routing := "*.messages"
+
+	log.Printf("Binding queue %s to exchange %s with routing key %s", q.Name, "imgs_topic", routing)
+	err = ch.QueueBind(
+		q.Name,       // queue name
+		routing,      // routing key
+		"imgs_topic", // exchange
+		false,
+		nil)
+
+	if err != nil {
+		fmt.Println("Error7:", err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // args
+	)
+
+	if err != nil {
+		fmt.Println("Error8:", err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		var m Message
+		for d := range msgs {
+			err = json.Unmarshal(d.Body, &m)
+
+			//log.Printf(" [x] %s", d.Body)
+			log.Printf(">%s", m.UserID)
+			log.Printf(">>%s", m.Key)
+			log.Printf(">>>%s", m.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+
+}
